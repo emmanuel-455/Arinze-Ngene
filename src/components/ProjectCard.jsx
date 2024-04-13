2c832b747709411d5ae657d643f61e8e01e52810
emmanuel-455 <120824375+emmanuel-455@users.noreply.github.com>
2024-04-13 17:53:49 +0100

    update

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -5,9 +5,9 @@ function ProjectCard({image, site, source }) {
     <div className='p-5'>
       <img className='w-[450px] h-[160px]' src={image} alt="" />
       <div className=' w-[300px]'>
-        <div className='flex justify-between'>
-          <button><a href={site}>View site</a></button>
-          <button><a href={source}>View source code</a></button>
+        <div className='flex mt-3 justify-between'>
+          <button className='border border-arinze-blue rounded-lg px-2 py-1'><a href={site}>View site</a></button>
+          <button className='border border-arinze-blue rounded-lg px-2 py-1'><a href={source}>View source code</a></button>
         </div>
      </div>
     </div>
