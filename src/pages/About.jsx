bc9da970de342d6966e841b79f9d3413a9945a58
emmanuel-455 <120824375+emmanuel-455@users.noreply.github.com>
2024-04-13 22:43:21 +0100

    updates

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Award from "../asset/Award.png"
 
 function About() {
   return (
@@ -18,7 +19,52 @@ function About() {
           <p className="text-[15px] text-A-writeup">100+ World Wide</p>
         </div>
       </div>
-      <p className="md:w-[60%] p-4 text-[15px] md:mb-[200px] mb-28 text-A-writeup text-center m-auto">Experienced and results-oriented Frontend Developer, having 2 years of experience. Offering a strong attention to details and accuracy, excellent problem-solving skills, and the important ability to work in fast-paced team environment. I am the reliable individual with a determination to meet and exceed all the assigned results </p>
+
+      <div className='md:flex md:justify-between px-6 md:px-[150px]'>
+        <div className=' md:w-[50%] mb-10'>
+        <div className='border-b mb-3 text-lg border-arinze-blue'>
+          <h1 className='font-semibold'>Introduction</h1>
+          </div>
+          <div className='text-[15px] text-A-writeup pr-1'>
+          <p className='mb-3 md:mb-5'>Hey there! I'm Ngene Arinze Emmanuel, a Web Designer with a passion for crafting immersive digital experiences. Over the past decade, I've delved into the world of web design, blending creativity and technical finesse to weave digital magic.</p>
+          <p className='mb-3 md:mb-5'>My portfolio speaks volumes about my dedication to creating visually stunning websites that not only catch the eye but also elevate user experiences. From startups to established brands, I've collaborated on projects showcasing my ability to translate ideas into functional, responsive, and visually captivating digital spaces.</p>
+          <p className='mb-3 md:mb-5'>Staying attuned to design trends, I ensure that my work has a timeless quality while incorporating the latest tools and technologies. With a toolbox filled with design tools and technologies, I effortlessly bring conceptual visions to life.</p>
+          <p className='mb-3 md:mb-5'>Beyond aesthetics, my two years of experience have honed my skills in navigating the dynamic tech landscape. Whether it's intuitive navigation, seamless interactivity, or optimizing performance for various devices, I've got it covered.</p>
+          <p className='mb-3 md:mb-5'>Explore my portfolio, and you'll uncover a decade of innovation, creativity, and a relentless commitment to transforming ideas into digital masterpieces. I don't just meet expectations; I exceed them, leaving a lasting impression on the virtual canvas. Welcome to my world, where design meets functionality, and every click is a step into a memorable online journey.</p>
+          </div>
+        </div>
+        <div className='w-[1px] mx-4 hidden md:block bg-A-writeup h-auto'>
+        </div>
+
+        <div className='md:w-[50%]'>
+        <div className='border-b mb-3 text-lg border-arinze-blue'>
+          <h1 className='font-semibold'>Awards</h1>
+          </div>
+
+          <div>
+            <div className='flex justify-between'>
+              <p className=' text-nowrap'>1. Certification</p>
+              <div className='flex mb-1 items-center justify-end'><img className='w-[12%] mr-1' src={Award} alt="" /><a className=' text-sm text-A-writeup text-nowrap' href="">Udemy Certificate</a></div>
+            </div>
+            <div className='flex mb-1 justify-between'>
+              <p className=' text-nowrap'>2. Open Source</p>
+              <div className='flex items-center justify-end'><img className='w-[12%] mr-1' src={Award} alt="" /><a className=' text-sm text-A-writeup text-nowrap' href="">Airbnb/JavaScript</a></div>
+            </div>
+            <div className='flex mb-1 justify-between'>
+              <p className=' text-nowrap'>3. Community Involvement</p>
+              <div className='flex items-center justify-end'><img className='w-[12%] mr-1' src={Award} alt="" /><a className=' text-sm text-A-writeup text-nowrap' href="">Centadesk</a></div>
+            </div>
+            <div className='flex mb-1 justify-between'>
+              <p className=' text-nowrap'>4. Personal Projects</p>
+              <div className='flex items-center justify-end'><img className='w-[12%] mr-1' src={Award} alt="" /><a className=' text-sm text-A-writeup text-nowrap' href="">Projects-Github</a></div>
+            </div>
+            <div className='flex mb-1 justify-between'>
+              <p className='text-nowrap'>5. Website of the year</p>
+              <div className='flex items-center justify-end'><img className='w-[12%] mr-1' src={Award} alt="" /><a className=' text-sm text-A-writeup text-nowrap' href="">Portfolio Award, 2024</a></div>
+            </div>
+          </div>
+          </div>
+      </div>
     </div>
   )
 }
