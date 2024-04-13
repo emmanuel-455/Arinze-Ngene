3fb59d78f34bcb01bfd2677db9969f19b489d140
emmanuel-455 <120824375+emmanuel-455@users.noreply.github.com>
2024-04-13 20:36:59 +0100

    update

--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -12,7 +12,7 @@ function Navbar() {
       initial={{y: '-100vh'}}
       animate={{ y: 0 }}
       transition={{delay: 0.5, duration: 0.5,type: 'spring', stiffness: 50}}
-      className='flex justify-between mb-10 relative px-10 border-b top-7 md:top-0 md:px-[70px] border-arinze-blue py-[60px] md:py-[6px] items-center'>
+      className='flex justify-between mb-10 relative px-10 border-b top-3 md:top-0 md:px-[70px] border-arinze-blue py-[60px] md:py-[6px] items-center'>
       <div className='md:flex flex-col hidden justify-center items-center'>
         <div className='text-[16px] text-A-writeup '><p className=' text-nowrap'>Let’s Connect:</p>
           <div className='flex w-6'>
@@ -25,8 +25,8 @@ function Navbar() {
       <div className='flex absolute top-0 md:relative items-center justify-center'>
       <img className='md:w-[7%] w-[16%] mr-3 rounded-full' src={PP} alt="" />
         <div>
-          <p className=' text-lg font-medium'>Ngene Arinze Emmanuel</p>
-          <p className="text-A-writeup">Frontend Developer</p>
+          <p className=' text-[18px] font-medium'>Ngene Arinze Emmanuel</p>
+          <p className="text-A-writeup text-sm">Frontend Developer</p>
         </div>
       </div>
       <div className='top-[80px] md:top-0 absolute md:relative left-[0px] px-[20px] md:left-[0px] right-[0px] flex md:block justify-between items-center'>
