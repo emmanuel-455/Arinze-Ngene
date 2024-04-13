2783c923665e9c306f2bd11d02da34ef53d4b0a7
emmanuel-455 <120824375+emmanuel-455@users.noreply.github.com>
2024-04-13 17:43:28 +0100

    update

--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -10,7 +10,7 @@ function Navbar() {
       transition={{delay: 0.5, duration: 0.5,type: 'spring', stiffness: 50}}
       className='flex justify-between px-4 border-b md:border-b-0 border-arinze-blue py-1 items-center md:py-5 md:px-11'>
       <div>
-       <img className='w-[30%]' src={logo} alt="" />
+       <img className='w-[25%]' src={logo} alt="" />
       </div>
       <div className='hidden md:block font-medium text-[15px]'>
         <a href="#Info" className='px-4'>Info</a>
@@ -19,7 +19,7 @@ function Navbar() {
         <a href="#Project" className='px-4'>Projects</a>
         <a href="#Media" className='px-4'>Media</a>
       </div>
-      <button className='bg-arinze-blue text-black px-5 py-[6px] font-medium md:text-[15px] md:py-2 md:px-8 rounded-3xl'>Contact info</button>
+      <button className='bg-arinze-blue text-black px-5 py-[6px] font-medium md:text-[14px] md:py-2 md:px-8 rounded-3xl'>Contact info</button>
     </motion.div>
   )
 }
