544b9dcc54800a3571385c90404bee75d4c32113
emmanuel-455 <120824375+emmanuel-455@users.noreply.github.com>
2024-04-14 02:42:56 +0100

    update

--- src/pages/Info.jsx
+++ src/pages/Info.jsx
@@ -18,23 +18,12 @@ function Info() {
   return (
     <div className='bg-black relative mb-[100px]' id="Info">
       <div>
-        <div className="flex justify-between">
-          <div>
-          <motion.button
-          initial={{ y: '100vh' }}
-          animate={{ y: 0 }}
-          transition={{ delay: -0.1, type: 'spring', stiffness: 15 }}
-          whileHover={{ scale: 1.1, boxShadow: "0px 0px 8px rgb(114,203,231)", textShadow: "0px 0px 2px rgb(114,203,231)"}}
-  class="cursor-pointer flex border-2 border-arinze-blue pl-1 pr-3 py-1 ml-3 items-center rounded-md text-arinze-blue m-auto tracking-wider shadow-xl"
->
- 
-          <a href="https://drive.google.com/file/d/1pGNSQjPPmNV9ixgm1VrpDjKKaQVSako1/view?usp=sharing" className='ml-2 text-arinze-blue font-medium text-[14px]'>Download Resume</a>
-</motion.button>
-          </div>
+        
+          
         <div className='flex relative md:hidden md:mr-8 mr-6 mt-3 justify-end'>
           <img src={Open} className='w-7 open fixed mb-3' onClick={openIcon} alt="" />
         </div>
-        </div>
+        
 
         <div style={{ display: open ? "block" : "none" }} className="w-full mt-[-40px] h-lvh font-medium text-xl fixed z-50 bg-black" id="bar">
           <div className="flex absolute right-0 mr-6 justify-end">
@@ -55,11 +44,22 @@ function Info() {
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
           transition={{ delay: 1, duration: 2 }}
-          className='mt-5 md:mt-0  md:pt-14 pb-16 border-b border-A-writeup pt-[40px]'>
+          className='mt-5 md:mt-0  md:pt-14 pb-[40px] border-b border-A-writeup pt-[40px]'>
           <p className='font-semibold md:text-[65px] text-[45px] leading-[50px] md:leading-snug mb-7 text-center m-auto  md:w-[70%] px-3 mt-4'>crafting immersive and visually stunning web experiences</p>
-          <div className="md:flex md:justify-center md:items-center">
+          <div className="md:flex md:justify-center mb-9 md:items-center">
             <p className='md:text-[20px] text-[15px] text-A-writeup font-normal'>Interested in working with me? </p><a className="text-[19px] flex justify-center font-medium items-center ml-2" href="">Book a Call <img className="ml-2 text-[20px]" src={Arrow} alt="" /></a>
           </div>
+          <div className="flex justify-center">
+  <motion.button
+    initial={{ y: '100vh' }}
+    animate={{ y: 0 }}
+    transition={{ delay: -0.1, type: 'spring', stiffness: 15 }}
+    whileHover={{ scale: 1.1, boxShadow: "0px 0px 8px rgb(114,203,231)", textShadow: "0px 0px 2px rgb(114,203,231)"}}
+    className="cursor-pointer flex border-[1px] border-arinze-blue pl-3 pr-5 py-2 ml-3 items-center rounded-md text-arinze-blue tracking-wider shadow-xl"
+  >
+    <a href="https://drive.google.com/file/d/1pGNSQjPPmNV9ixgm1VrpDjKKaQVSako1/view?usp=sharing" className='ml-2 text-arinze-blue font-medium text-[14px]'>Download Resume</a>
+  </motion.button>
+</div>
         </motion.div>
         {/* <motion.button
           initial={{ y: '100vh' }}
