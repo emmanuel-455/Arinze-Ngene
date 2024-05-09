37ca44ad0664bea9b5ba2e1b6e54b761e771fa38
emmanuel-455 <120824375+emmanuel-455@users.noreply.github.com>
2024-05-09 17:01:50 +0100

    update

--- src/pages/BlogPage.jsx
+++ src/pages/BlogPage.jsx
@@ -10,7 +10,7 @@ function BlogPage() {
   async function getBlogs() { 
     try {
       const apiKey = 'b27502994f9247f986a13f3c6f0c9aea';
-      const response = await fetch(`https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=${apiKey}`);
+      const response = await fetch(`http://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=${apiKey}`);
       const data = await response.json();
 
       // Limit content characters to 200 characters
