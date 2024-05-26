cb68ba1d820833713eab54a8ad4dcea2d5d13b43
emmanuel-455 <120824375+emmanuel-455@users.noreply.github.com>
2024-05-26 12:13:57 +0100

    update

--- src/ProjectContent.js
+++ src/ProjectContent.js
@@ -10,78 +10,85 @@ import Drum from "./asset/Drum.png"
 import Searcher from "./asset/Searcher.png"
 import Scorespot from "./asset/Scorespot.png"
 import MovieTrack from "./asset/MovieTrack.png"
+import MovieMaze from "./asset/MMP.png"
 
 const ProjectContent = [
   {
     id: 1,
+    image: MovieMaze,
+    site: "https://movie-maze-theta.vercel.app/Home",
+    source: "https://github.com/emmanuel-455/movie-maze"
+  },
+  {
+    id: 2,
     image: MovieTrack,
     site: "https://movietrack-delta.vercel.app",
     source: "https://github.com/emmanuel-455/Movietrack"
   },
   {
-    id: 2,
+    id: 3,
     image: Scorespot,
     site: "https://score-spot.vercel.app/",
     source: "https://github.com/emmanuel-455/ScoreSpot"
   },
   {
-    id: 3,
+    id: 4,
     image: Shazam,
     site: "https://shazamify-nu.vercel.app/",
     source: "https://github.com/emmanuel-455/shazam-api-helper"
   },
   {
-    id: 4,
+    id: 5,
     image: daluWorkImage,
     site: "http://dalu-psi.vercel.app",
     source: "https://github.com/emmanuel-455/dalu"
   },
   {
-    id: 5,
+    id: 6,
     image: nimoImage,
     site: "https://arinze-ngene.vercel.app",
     source: "https://github.com/emmanuel-455/Arinze-Ngene"
   },
   {
-    id: 6,
+    id: 7,
     image: gpt3Image,
     site: "https://gpt3-reactjs-website-project.vercel.app",
     source: "https://github.com/emmanuel-455/gpt3_Reactjs_website_project"
   },
   {
-    id: 7,
+    id: 8,
     image: restauImage,
     heading: "Gericht Restauran (ReactjS)",
     site: "https://my-restaurant-gpt3.vercel.app/",
     source: "https://github.com/emmanuel-455/drum_kit_game_js"
   },
   {
-    id: 8,
+    id: 9,
     image: ticTacToeImage,
     site: "https://nimos-project-websites.on.drv.tw/Tic tac toe game/",
     source: "https://github.com/emmanuel-455/tic_tac_toe_game_js"
   },
   
   {
-    id: 9,
+    id: 10,
     image: Simon,
     site: "https://nimos-project-websites.on.drv.tw/Simon%20Game%20JS%20project/",
     source: "https://nimos-project-websites.on.drv.tw/Simon%20Game%20JS%20project/"
   },
   {
-    id: 10,
+    id: 11,
     image: Dice,
     site: "https://nimos-project-websites.on.drv.tw/Dicee%20game%20Js%20project/dicee.html",
     source: "https://github.com/emmanuel-455/dice_game"
   },
   {
-    id: 11,
+    id: 12,
     image: Drum,
     site: "https://nimos-project-websites.on.drv.tw/Drum Kit js/",
     source: "https://github.com/emmanuel-455/dice_game"
   },
   {
-    id: 12,
+    id: 13,
     image: Searcher,
     site: "https://nimos-project-websites.on.drv.tw/image_searcher%20project%20js/",
     source: "https://github.com/emmanuel-455/unsplash_api_image_search_app"
